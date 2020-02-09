2bf359ca158bf224107a95b34e100a2bec09f271
cr00za <52474968+cr00za@users.noreply.github.com>
2020-02-09 15:18:28 -0500

    added change of instruction block
    - added change of instruction block

--- js/interaction.js
+++ js/interaction.js
@@ -9,7 +9,7 @@
       $('.carousel-indicators li a img').click(function() {
          let alt = $(this).attr('alt');
          $('.material-title').html(alt);
-         $('.instruct-block-title').replace("Fluorescent Bulb", "alt");
+         $('.instruct-block-title').swap("Fluorescent Bulb", "alt");
        });
 
   });
@@ -17,3 +17,33 @@
 //   $(".instruct-block-title").text(function () {
 //    return $(this).text().replace("contains", "hello everyone"); 
 // });​​​​​
+
+function changeBlock1() {
+   document.getElementById("div1").style.display="block";
+   document.getElementById("div2").style.display="none";
+   document.getElementById("div3").style.display="none";
+   document.getElementById("div4").style.display="none";
+   }
+
+function changeBlock2() {
+   document.getElementById("div1").style.display="none";
+   document.getElementById("div2").style.display="block";
+   document.getElementById("div3").style.display="none";
+   document.getElementById("div4").style.display="none";
+   }
+
+function changeBlock3() {
+   document.getElementById("div1").style.display="none";
+   document.getElementById("div2").style.display="none";
+   document.getElementById("div3").style.display="block";
+   document.getElementById("div4").style.display="none";
+   }
+
+function changeBlock4() {
+   document.getElementById("div1").style.display="none";
+   document.getElementById("div2").style.display="none";
+   document.getElementById("div3").style.display="none";
+   document.getElementById("div4").style.display="block";
+   }
+   
+   
